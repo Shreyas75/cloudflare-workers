bcf8e818579da1dac1bfa2ca3d0ec39568009acd
Shreyas75 <manotishreyas@gmail.com>
2024-07-25 08:51:44 +0530

    Cloudflare workers

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return Response.json({message: 'Hello World!'});
 	},
 } satisfies ExportedHandler<Env>;
